66c3d3604f41d859afd6d51e7b0b81a68c3b6440
Jethro Manuel <jethromanuel0519@gmail.com>
2026-05-30 21:48:25 +0800

    modified:   admin/add_document.php 	modified:   admin/add_user.php

--- admin/add_document.php
+++ admin/add_document.php
@@ -3,7 +3,6 @@ session_start();
 
 include('../config/connect.php');
 include('../middleware/auth.php');
-include('../includes/csrf.php');
 
 $user = require_role(['admin']);
 enforce_password_change($user);
